d5089dda1a879386987cb34e6b11e06a4c756adb
xxkun <332271571@qq.com>
2020-09-16 18:55:07 +0800

    shellsort

--- Java/Sort/ShellSort.java
+++ Java/Sort/ShellSort.java
@@ -5,16 +5,20 @@ public class ShellSort{
     public static <T extends Comparable<? super T>> void shellSort(T[] arr){
         int j;
         for(int gap=arr.length/2;gap>0;gap/=2){
-            for(int i=1;i<arr.length;i+=gap){
-                j=i;
-                while(j>0&&arr[j].compareTo(arr[--j])<0){
-                    
+            for(int i=gap;i<arr.length;i++){
+                T tmp=arr[i];
+                for(j=i;j-gap>=0 && tmp.compareTo(arr[j-gap])<0;j-=gap){
+                    arr[j]=arr[j-gap];
                 }
+                arr[j]=tmp;
             }
         }
     }
 
     public static void main(String[] args) {
-        
+        Integer[] arr={0,31,53,59,88,41,58,57,97};
+        System.out.println(Arrays.asList(arr).toString());
+        shellSort(arr);
+        System.out.println(Arrays.asList(arr).toString());
     }
 }
