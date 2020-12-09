08b27ac83a9896a0a746d8e38de457338d77c8e7
xxkun <332271571@qq.com>
2020-12-09 11:03:04 +0800

    add mall

---
+++ Java/IDEA/ch-02/src/main/java/com/orange/service/impl/SomeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.orange.service.impl;
+
+import com.orange.service.SomeService;
+
+public class SomeServiceImpl implements SomeService {
+    @Override
+    public void doSome() {
+        System.out.println("Success!");
+    }
+}
