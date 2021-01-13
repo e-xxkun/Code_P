5c04d0490a165fea8aac74409bb6f3dc4411929a
e-xxkun <332271571@qq.com>
2021-01-13 22:07:32 +0800

    merge Mall RabbitMQ

---
+++ Java/IDEA/Mall/src/main/java/com/orange/mall/service/OmsPortalOrderService.java
@@ -0,0 +1,20 @@
+package com.orange.mall.service;
+
+import com.orange.mall.common.CommonResult;
+import com.orange.mall.dto.OrderParam;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface OmsPortalOrderService {
+
+    /**
+     * 根据提交信息生成订单
+     */
+    @Transactional
+    CommonResult generateOrder(OrderParam orderParam);
+
+    /**
+     * 取消单个超时订单
+     */
+    @Transactional
+    void cancelOrder(Long orderId);
+}
