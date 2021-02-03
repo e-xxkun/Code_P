3661e727e2cf463284c9898548a3cf5b3aa03c81
xxkun <332271571@qq.com>
2021-02-03 17:16:12 +0800

    P2P restart

--- Java/Test/main/Main.java
+++ Java/Test/main/Main.java
@@ -1,7 +1,9 @@
-package main;
+// package main;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("success");
+        int[] a = new int[]{2,3,4,5,6};
+        int i = 2;
+        System.out.println(a[i] - a[i ++]);
     }
 }
